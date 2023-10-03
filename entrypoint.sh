161dcb76d49a5d5a511b96c68658d9a382e3827f
cedricd <cedricdecruw@gmail.com>
2023-10-03 19:26:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,22 @@
 pwd
 ls -a
 cd ../..
-ls -a
 echo "extra_metadata is" $1
 #install pip requirements
 pip install -r requirements.txt --no-cache-dir
+
+#copy over main.py and /src/rocrate.py to the github workspace
+cp main.py github/workspace
+cp src/rocrate.py github/workspace
+
 cd github/workspace
+ls -al
 # copy over the extra metadata file if it exists to the main directory
 if [ -f $1 ]; then
     cp $1 extra_metadata.json
     #run the main python file
-    cd ../..
     python main.py --extra_metadata extra_metadata.json
 else
     #run the main python file
-    cd ../..
     python main.py
 fi

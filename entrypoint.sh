00e596d1e3b7f0e45bd2ef593ff62174d38f712e
cedricd <cedricdecruw@gmail.com>
2023-10-03 17:46:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,12 @@ pip install -r requirements.txt --no-cache-dir
 
 # copy over the extra metadata file if it exists to the main directory
 if [ -f $1 ]; then
-    cp $1 /github/workspace/extra_metadata.json
+    cp $1 extra_metadata.json
     #run the main python file
+    cd ../..
     python main.py --extra_metadata extra_metadata.json
 else
     #run the main python file
+    cd ../..
     python main.py
 fi
